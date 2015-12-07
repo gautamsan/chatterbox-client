385be43fcaf04ad932549d419fbb2247bce9fb57
Santosh Gautam <santoshgautam7@gmail.com>
2015-12-07 15:50:33 -0800

    Added dependencies in index.html and implemented post and fetch methods in app.js.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,35 @@
-// YOUR CODE HERE:
+var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox' //used for all requests
+app.init = function() {
+
+}
+
+app.send = function(message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent. Data: ', data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message. Error: ', data);
+    }
+  });
+}
+
+app.fetch = function() {
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received. Data: ', data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to receive message. Error: ', data);
+    }
+  });
+}
 
