cb4c87f7886e60710864b64b6d93e28f8383ef7a
Santosh Gautam <santoshgautam7@gmail.com>
2015-12-07 16:47:39 -0800

    Added add and clear methods in app.js.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,14 @@
 var app = {};
 app.server = 'https://api.parse.com/1/classes/chatterbox' //used for all requests
 app.init = function() {
-
-}
+  // app.addMessage({
+  //   username: 'Mel Brooks',
+  //   text: 'Never underestimate the power of the Schwartz!',
+  //   roomname: 'lobby',
+  //   updatedAt: "2015-09-01T01:00:42.028Z"
+  // });
+  // app.clearMessages();
+};
 
 app.send = function(message) {
   $.ajax({
@@ -33,3 +39,20 @@ app.fetch = function() {
   });
 }
 
+app.clearMessages = function() {
+  $('#chats').html('');
+}
+
+app.addMessage = function(message) {
+  var $chats = $('#chats');
+  var newChat = message.text;
+  $chats.append('<div>' + newChat + '</div');
+}
+
+app.addRoom = function() {
+  
+}
+
+$(document).ready(function(){
+  app.init();
+});
